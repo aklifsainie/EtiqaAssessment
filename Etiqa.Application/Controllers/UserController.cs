15ef25e39c6d3f43dbdcea7ea554967395b99248
aklifsainie <aklifsainie@gmail.com>
2023-12-14 16:01:26 +0800

    Add Edit and Delete view

--- Etiqa.Application/Controllers/UserController.cs
+++ Etiqa.Application/Controllers/UserController.cs
@@ -66,6 +66,7 @@ namespace Etiqa.Application.Controllers
             
         }
 
+        [HttpGet]
         public async Task<IActionResult> UserDetail(string username)
         {
             UserViewModel userDetail = new UserViewModel();
@@ -75,6 +76,81 @@ namespace Etiqa.Application.Controllers
             {
                 string data = response.Content.ReadAsStringAsync().Result;
                 userDetail = JsonConvert.DeserializeObject<UserViewModel>(data);
+            }
+
+            return View(userDetail);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string username)
+        {
+            UserViewModel userDetail = new UserViewModel();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/User/GetDetail?username=" + username).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                userDetail = JsonConvert.DeserializeObject<UserViewModel>(data);
+            }
+
+            return View(userDetail);    
+        }
+
+        [HttpPost]
+        public IActionResult Edit(UserViewModel user)
+        {
+            try
+            {
+                //UserViewModel userList = new UserViewModel();
+
+                string data = JsonConvert.SerializeObject(user);
+                StringContent content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = _client.PutAsync(_client.BaseAddress + "/User/UpdateUser", content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["success"] = "User updated";
+                    return RedirectToAction(nameof(Index));
+                }
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                return View();
+            }
+
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(string username)
+        {
+            UserViewModel userDetail = new UserViewModel();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/User/GetDetail?username=" + username).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                userDetail = JsonConvert.DeserializeObject<UserViewModel>(data);
+            }
+
+            return View(userDetail);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(string username)
+        {
+            UserViewModel userDetail = new UserViewModel();
+            HttpResponseMessage response = _client.DeleteAsync(_client.BaseAddress + "/User/DeleteUser?username=" + username).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                userDetail = JsonConvert.DeserializeObject<UserViewModel>(data);
+                return RedirectToAction(nameof(Index));
             }
 
             return View(userDetail);
